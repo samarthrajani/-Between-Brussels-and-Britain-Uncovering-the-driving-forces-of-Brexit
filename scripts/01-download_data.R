5790133306a314a1b0eddbd338bd64c414754db9
Samarth Rajani <samarth.rajani@mail.utoronto.ca>
2024-03-28 16:54:40 -0400

    Tried to donwload dataset. Taking too long

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -9,18 +9,17 @@
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(dataverse)
 
-#### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
-
+# Load the resource data into a tibble
+raw_data_UK <- get_dataframe_by_name(filename= "ParlEE_UK_plenary_speeches.csv", dataset = "10.7910/DVN/ZY3RV7", server = "dataverse.harvard.edu", .f = read_csv) %>%
+  slice_head(n = 10) %>% select(date, agenda, party, policyarea)
+raw_data_UK
 
 #### Save data ####
 # [...UPDATE THIS...]
 # change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+#write_csv(the_raw_data, "inputs/data/raw_data.csv") 
 
          
