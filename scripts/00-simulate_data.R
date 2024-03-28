4ad53eebbebe66a1567b383ccd12f9768a20ffda
Samarth Rajani <samarth.rajani@mail.utoronto.ca>
2024-03-28 14:53:01 -0400

    Simulated data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,20 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates occurence of environmental matter
+# Author: Samarth Rajani
+# Date: 28 March 2024 
+# Contact: samarth.rajani@mail.utoronto.ca 
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+years <- rep(seq(2009, 2019, by = 1), each = 10)
+agenda_type <- sample(c("Education","Environment","Agriculture","Immigration","Crime"),replace = TRUE, size = 110)
+party <- sample(c("APNI", "Conservative Party","Labour Party", "LibDem","Other","UUP"), replace = TRUE, size = 110)
 
+Simulated_data <- data.frame(Year = years, Agenda = agenda_type, Party = party)
+Simulated_data
 
-
+### Saving simulated data ###
+write_csv(simulated_data, "data/simulated_data.csv")
