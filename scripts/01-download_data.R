cb23fa79100f1bc6ad0db7ac8e89bacef2db2d8c
Samarth Rajani <samarth.rajani@mail.utoronto.ca>
2024-04-16 23:58:45 -0400

    Few changes

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,11 +1,10 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from the ParlEE dataset
+# Author: Samarth Rajani
+# Date: 16 April 2024
+# Contact: samarth.rajani@utoronto.ca 
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+
 
 
 #### Workspace setup ####
@@ -19,43 +18,11 @@ raw_data_UK_edited <- get_dataframe_by_name(
   server = "dataverse.harvard.edu",
   .f = read_csv
 ) %>%
-  filter(chair == FALSE) %>%
+  filter(chair == FALSE, agenda != "Election of Speaker") %>%
   select(date, agenda, party, policyarea, eu)
 
-# Save data individually by year and save it
+# Save data (locally since it is too big to be pushed)
 
-raw_data_UK_2009 <- filter(raw_data_UK_edited, year(date) == 2009)
-write_csv(raw_data_UK_2009, "data/raw_data/raw_data_2009.csv")
-
-raw_data_UK_2010 <- filter(raw_data_UK_edited, year(date) == 2010)
-write_csv(raw_data_UK_2010, "data/raw_data/raw_data_2010.csv")
-
-raw_data_UK_2011 <- filter(raw_data_UK_edited, year(date) == 2011)
-write_csv(raw_data_UK_2011, "data/raw_data/raw_data_2011.csv")
-
-raw_data_UK_2012 <- filter(raw_data_UK_edited, year(date) == 2012)
-write_csv(raw_data_UK_2012, "data/raw_data/raw_data_2012.csv")
-
-raw_data_UK_2013 <- filter(raw_data_UK_edited, year(date) == 2013)
-write_csv(raw_data_UK_2013, "data/raw_data/raw_data_2013.csv")
-
-raw_data_UK_2014 <- filter(raw_data_UK_edited, year(date) == 2014)
-write_csv(raw_data_UK_2014, "data/raw_data/raw_data_2014.csv")
-
-raw_data_UK_2015 <- filter(raw_data_UK_edited, year(date) == 2015)
-write_csv(raw_data_UK_2015, "data/raw_data/raw_data_2015.csv")
-
-raw_data_UK_2016 <- filter(raw_data_UK_edited, year(date) == 2016)
-write_csv(raw_data_UK_2016, "data/raw_data/raw_data_2016.csv")
-
-raw_data_UK_2017 <- filter(raw_data_UK_edited, year(date) == 2017)
-write_csv(raw_data_UK_2017, "data/raw_data/raw_data_2017.csv")
-
-raw_data_UK_2018 <- filter(raw_data_UK_edited, year(date) == 2018)
-write_csv(raw_data_UK_2018, "data/raw_data/raw_data_2018.csv")
-
-raw_data_UK_2019 <- filter(raw_data_UK_edited, year(date) == 2019)
-write_csv(raw_data_UK_2019, "data/raw_data/raw_data_2019.csv")
-
+write_csv(raw_data_UK_edited, "/Users/Samarthsumitrajani/Final paper/data/raw_data/raw_data.csv")
 
          
--- scripts/04-model.R
+++ scripts/04-model.R
@@ -1,11 +1,9 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from the ParlEE dataset
+# Author: Samarth Rajani
+# Date: 16 April 2024
+# Contact: samarth.rajani@utoronto.ca 
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
 
 
 #### Workspace setup ####
@@ -13,48 +11,16 @@ library(tidyverse)
 library(rstanarm)
 
 #### Read data ####
-# Read in all the data
-data_2009 <- read_csv("~/Final paper/data/raw_data/raw_data_2009.csv")
-data_2010 <- read_csv("~/Final paper/data/raw_data/raw_data_2010.csv")
-data_2011 <- read_csv("~/Final paper/data/raw_data/raw_data_2011.csv")
-data_2012 <- read_csv("~/Final paper/data/raw_data/raw_data_2012.csv")
-data_2013 <- read_csv("~/Final paper/data/raw_data/raw_data_2013.csv")
-data_2014 <- read_csv("~/Final paper/data/raw_data/raw_data_2014.csv")
-data_2015 <- read_csv("~/Final paper/data/raw_data/raw_data_2015.csv")
-data_2016 <- read_csv("~/Final paper/data/raw_data/raw_data_2016.csv")
-data_2017 <- read_csv("~/Final paper/data/raw_data/raw_data_2017.csv")
-data_2018 <- read_csv("~/Final paper/data/raw_data/raw_data_2018.csv")
-data_2019 <- read_csv("~/Final paper/data/raw_data/raw_data_2019.csv")
 
-### Create one table and filter by environment
-
-combined_data <- rbind(data_2009, data_2010, data_2011, data_2012, data_2013, data_2014, data_2015, data_2016, data_2017, data_2018, data_2019)
-
-environment_data_year <- combined_data %>% filter(policyarea == 7)%>% mutate(year = year(date))
-
-## Create counts
-
-count_data_year <- environment_data_year %>% group_by(year) %>% summarise(count = n())
+all_data <- read_csv("/Users/Samarthsumitrajani/Final paper/data/raw_data/raw_data.csv")
 
 ### Model data by year ####
-first_model <-
-  stan_glm(
-    formula = count ~ year,
-    data = count_data,
-    family = neg_binomial_2(link = "log"),
-    seed = 853
-  )
-first_model
+first_model <- glm(eu ~ party, data = all_data, family = binomial)
+summary(first_model)
+saveRDS(
+  first_model,
+  file = "models/first_model.rds"
+)
 
-### Model by month 
 
-#### Save model ####
-#saveRDS(
-#  first_model,
-#  file = "models/first_model.rds"
-#)
-
-#prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-#prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-#prior_aux = exponential(rate = 1, autoscale = TRUE),
 
