485211cff987a728c138e4d7ed1a2a301bb4d1a5
Samarth Rajani <samarth.rajani@mail.utoronto.ca>
2024-04-19 02:40:43 -0400

    Modelsumary worked

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,29 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Tests the dataset
+# Author: Samarth Rajani
+# Date: 16 April 2024
+# Contact: samarth.rajani@utoronto.ca 
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Test data ####
+dataset_test <- read_csv("/Users/Samarthsumitrajani/Final paper/data/raw_data/raw_data.csv")
+
+# Test for missing values
+is.na(cleaned_data)
+
+# Test whether EU column is only 0 and 1 
+cleaned_data$eu|>
+  unique() == c(0:1)
+
+# Test whether the party names are those indeed present in UK parliament
+
+cleaned_data$party|>
+  unique() == c('APNI', 'Con', 'DUP', 'GPEW',
+                'Lab', 'LibDem', 'Plaid Cymru',
+                'SDLP', 'SNP', 'UKIP', 'UUP')
+
+
